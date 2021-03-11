0f8840afa20d36e3baac55cf209b0b1275c5862f
ko1105 <74762732+ko1105@users.noreply.github.com>
2021-03-11 19:58:34 +0900

    Add files via upload

---
+++ 16.プロパティ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Class1
+{
+	public string name1 { get; set; }
+	public string name2 { get;} = "あ";
+}
+class Test1
+{
+	static void Main()
+	{
+		var c1 = new Class1();
+		var c2 = new Class1();
+		var c3 = new Class1();
+
+		c1.name1 = "い";
+		c2.name1 = "う";
+
+		Console.WriteLine(c1.name1);
+		Console.WriteLine(c2.name1);
+		Console.WriteLine(c3.name2);
+	}
+}
